1587339b9432ef6f5ef5c964e47d7606c5077eae
CANDELA Nicolas <darkxell.mc@gmail.com>
2021-10-26 13:10:04 +0200

    Added prism primitives
    Okay, enough now.

--- Assets/Buildings/Primitives/PrimitiveFactory.cs
+++ Assets/Buildings/Primitives/PrimitiveFactory.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 /// <summary>Type of primitive, generable by a <c>PrimitiveFactory</c></summary>
 public enum PrimitiveType {
-    Sphere, Isocaedron, Cube, Pyramid4, Cylindre, Cone, RoofSingle, RoofDouble
+    Sphere, Isocaedron, Cube, Pyramid4, Cylindre, Cone, RoofSingle, RoofDouble, TriPrism, PentaPrism, HexaPrism, OctoPrism
 }
 
 /// <summary>Factory for all Primitive mesh types, that may be used for composition.</summary>
@@ -53,16 +53,28 @@ public class PrimitiveFactory {
                 toreturn = PyramidBuilder.buildPyramidt1();
                 break;
             case PrimitiveType.Cylindre:
-                toreturn = CylindreBuilder.CreateCylinder(0.5f, 0.5f, 1, 12, 1, false);
+                toreturn = CylindreBuilder.CreateCylinder(0.5f, 0.5f, 1, 16, 1, false);
                 break;
             case PrimitiveType.Cone:
-                toreturn = CylindreBuilder.CreateCylinder(0, 0.5f, 1, 12, 1, false);
+                toreturn = CylindreBuilder.CreateCylinder(0, 0.5f, 1, 16, 1, false);
                 break;
             case PrimitiveType.RoofSingle:
                 toreturn = RoofBuilder.BuildRoof(false);
                 break;
             case PrimitiveType.RoofDouble:
                 toreturn = RoofBuilder.BuildRoof(true);
+                break;
+            case PrimitiveType.TriPrism:
+                toreturn = CylindreBuilder.CreateCylinder(0.5f, 0.5f, 1, 3, 1, false);
+                break;
+            case PrimitiveType.PentaPrism:
+                toreturn = CylindreBuilder.CreateCylinder(0.5f, 0.5f, 1, 5, 1, false);
+                break;
+            case PrimitiveType.HexaPrism:
+                toreturn = CylindreBuilder.CreateCylinder(0.5f, 0.5f, 1, 6, 1, false);
+                break;
+            case PrimitiveType.OctoPrism:
+                toreturn = CylindreBuilder.CreateCylinder(0.5f, 0.5f, 1, 8, 1, false);
                 break;
         }
         geometryBuffer.Add(type, toreturn);
